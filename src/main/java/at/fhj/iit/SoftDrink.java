ceaff2bd3fa6f56cdafbaac4dbf303386db84fe5
dominik-github1 <80465736+dominik-github1@users.noreply.github.com>
2021-03-15 20:36:02 +0100

    making a softdrink method

--- src/main/java/at/fhj/iit/SoftDrink.java
+++ src/main/java/at/fhj/iit/SoftDrink.java
@@ -1,5 +1,13 @@
 package at.fhj.iit;
 
-public class SoftDrink extends SimpleDrink{
+public class SoftDrink extends Drink{
+
+    Drink cola = new Drink();
+    cola.name
+
+    public Drink mixingSoftDrink(Drink drink1, Drink drink2 ){
+        drink1
+
+    }
 
 }
