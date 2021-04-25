dad0b71ea29c073862ee61181ad5842890d88668
dominik bregovic <dominik.bregovic@edu.fh-joanneum.at>
2021-04-25 18:36:31 +0200

    added comments for documentation

--- src/main/java/at/fhj/iit/SoftDrink.java
+++ src/main/java/at/fhj/iit/SoftDrink.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 
 public class SoftDrink extends Drink{
 
-    // naming my drink and how much I want of it
+    /**
+     *
+     * @param name1 , softdrink name
+     * @param volume , volume in liters
+     *               the attributes that are necessary for a softdrink in order to exist
+     * alcoholic and alcoholicPercent are set this values because softdrinks are not alcoholic
+     */
     public SoftDrink(String name1, double volume){
         super.name = name1;
         super.volume = volume;
@@ -13,10 +19,20 @@ public class SoftDrink extends Drink{
         super.alcoholPercent = 0.0;
     }
 
+    /**
+     * default constuctor
+     */
     public SoftDrink(){
     }
 
 
+    /**
+     *
+     * @param name1 , first softdrink
+     * @param name2 , second softdrink
+     * if the combination of the two softdrinks is legal, then new softdrink is crated
+     * @return if valid mix returns the new creation, if not valid " No such combination in directory"
+     */
     public String mixSoftDrinks(String name1, String name2) {
         String mix = name1.trim().toLowerCase()+name2.trim().toLowerCase();
         if (mix.equals("colafanta") || mix.equals("fantacola")){
@@ -26,7 +42,7 @@ public class SoftDrink extends Drink{
         }else if (mix.equals("fantasoda") || mix.equals("sodafanta") ) {
             mix = "fanta with soda";
         }else {
-            mix = "No such kombination in base";
+            mix = "No such combination in base";
         }
 
         return mix;
