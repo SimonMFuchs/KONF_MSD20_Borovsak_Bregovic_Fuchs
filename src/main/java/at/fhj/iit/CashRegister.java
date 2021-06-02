0d597796b576355532dfce3b741b5fc5972ae32d
dominik-github1 <80465736+dominik-github1@users.noreply.github.com>
2021-06-02 14:58:17 +0200

    changed class to interface

--- src/main/java/at/fhj/iit/CashRegister.java
+++ src/main/java/at/fhj/iit/CashRegister.java
@@ -1,27 +1,9 @@
 package at.fhj.iit;
 
-public class CashRegister {
-    // all possible drinks:
-    
-    //we could take Softdrink as non-alk
-    SoftDrink softDrink;
-    //we could take wine as low-alk
-    Wine wine;
-    //we could take Simpledrink as high-alk
-    SimpleDrink simpleDrink;
+public interface CashRegister {
 
-
-    public void buySoftDrink(){
+    public void buyDrink(){
 
     }
 
-
-    public void buySimpleDrink(){
-
-    }
-
-
-    public void buyWine(){
-
-    }
 }
