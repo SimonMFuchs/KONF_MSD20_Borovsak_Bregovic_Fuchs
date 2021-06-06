f37e7312dc5a929c348f83a55848081f70a419a1
Simon Fuchs <simon.fuchs@edu.fh-joanneum.at>
2021-06-06 17:10:17 +0200

    changed testing class and updated md maven sites

--- src/test/java/at/fhj/iit/CashRegisterTest.java
+++ src/test/java/at/fhj/iit/CashRegisterTest.java
@@ -5,18 +5,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CashRegisterTest {
+public class CashRegisterTest {
+
+    private CashRegister cashRegister;
+    private Cashier cashier1;
+    private Cashier cashier2;
+    private Sale sale;
 
     @BeforeEach
     public void setup() {
+        cashier1 = new Cashier("testCashier");
+        cashRegister = new CashRegister(cashier1);
+        sale = new Sale(cashier1, 5, .40);
+        cashier2 = new Cashier("testCashier2");
     }
 
     @Test
     void changeCashier() {
+        cashRegister.addCashier(cashier2);
+        cashRegister.changeCashier(cashier2);
+        assertEquals(cashier2, cashRegister.getCurrentCashier());
     }
 
     @Test
     void addSale() {
+        cashRegister.addSale(sale);
     }
 
     @Test
@@ -49,5 +62,53 @@ class CashRegisterTest {
 
     @Test
     void getCurrentCashier() {
+    }
+
+    @Test
+    void testChangeCashier() {
+    }
+
+    @Test
+    void testAddSale() {
+    }
+
+    @Test
+    void testGetRevenueNonAlcoholic() {
+    }
+
+    @Test
+    void testGetRevenueLowAlcoholic() {
+    }
+
+    @Test
+    void testGetRevenueHighAlcoholic() {
+    }
+
+    @Test
+    void testGetRevenueByDay() {
+    }
+
+    @Test
+    void testGetRevenueByCashier() {
+    }
+
+    @Test
+    void testGetRevenueByCashierAndDay() {
+    }
+
+    @Test
+    void testGetCashierList() {
+    }
+
+    @Test
+    void testGetCurrentCashier() {
+    }
+
+    @Test
+    void addCashier() {
+    }
+
+    @Test
+    void getSalesList() {
     }
 }
